8f97b9601da4eb59c0101cd7eea6a9497e94c2f4
adrianrdguez <adrianrdgueznavas@gmail.com>
2025-03-24 16:37:57 +0000

    include validatio

--- src/modules/dogs/dogs.module.ts
+++ src/modules/dogs/dogs.module.ts
@@ -3,10 +3,21 @@ import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { DogsController } from './dogs.controller';
 import { DogsService } from './dogs.service';
+import { ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [HttpModule, ConfigModule],
   controllers: [DogsController],
-  providers: [DogsService],
+  providers: [
+    DogsService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        transform: true,
+      }),
+    },
+  ],
 })
 export class DogsModule {}
